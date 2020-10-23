a97f45933cad418467a5f1313efd5de0ff6e5cda
ss7sudesh <sudeshkumardas7@gmail.com>
2020-10-23 16:47:58 +0530

    Added Design

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,12 @@
-// get the grid-container. it is the parent container
+// get the elements
 const gridContainer = document.getElementById('gridContainer');
+const resetBtn = document.getElementById('resetBtn')
+const rgbBtn = document.getElementById('rgbBtn');
+const blackBtn = document.getElementById('blackBtn');
 
-// add css to conatiner 
-gridContainer.classList.add('flex-container');
+
+
+// functions
 
 //write function to make a grid
 function grid() {
@@ -10,15 +14,12 @@ function grid() {
     for(let i = 0; i < 256; i++) {
         let item = document.createElement('div')
         item.classList.add('gridItem')
-
-        // css for grid
         item.style.border = '1px solid white';
-
+        
+        // add to the container
         gridContainer.appendChild(item);
     }
 }
-// call the grid function
-grid()
 
 //hover to change background
 function hover() {
@@ -30,13 +31,6 @@ function hover() {
         })
     })
 }
-//calling hover function to draw
-hover()
-
-// get the button and
-const resetBtn = document.getElementById('resetBtn')
-resetBtn.addEventListener('click', resetFunc);
-resetBtn.addEventListener('click', newGrid);
 
 function resetFunc() {
     // getting the grid and reset color
@@ -47,14 +41,15 @@ function resetFunc() {
     });
 }
 
-
 function newGrid() {
     // get the grid
     const gridContainer = document.getElementById('gridContainer');
 
-    // change the grid size 
-    gridLength = parseInt(prompt('What would you like to be your grid size?'));
-
+    // change the grid size ask for size 
+    do{
+        gridLength = parseInt(prompt('What would you like to be your grid size? Choose a number betweeen 2 and 100', 10));    
+    }while(isNaN(gridLength) || gridLength < 2 || gridLength >100)
+    
     // deleting the old grid
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.lastChild);
@@ -72,15 +67,12 @@ function newGrid() {
         gridContainer.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;
         gridContainer.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`;
 
+        // adding the items to grid
         gridContainer.appendChild(item);
     }
-
+    // call hover to make it work after reset
     hover();
 }
-
-// random rgb button
-const rgbBtn = document.getElementById('rgbBtn');
-rgbBtn.addEventListener('click', randomRGB);
 
 // funtion to change random colour
 function randomRGB() {
@@ -97,6 +89,16 @@ function randomRGB() {
     })
 }
 
-// black button
-const blackBtn = document.getElementById('blackBtn');
+
+
+// call required functions
+grid()
+hover()
+
+
+
+// event listening to buttons
+resetBtn.addEventListener('click', resetFunc);
+resetBtn.addEventListener('click', newGrid);
+rgbBtn.addEventListener('click', randomRGB);
 blackBtn.addEventListener('click', hover);
